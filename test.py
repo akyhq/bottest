7c6db12a16cc0919b9af258f4a0fb7028bc5b24e
akyhq <156787006+akyhq@users.noreply.github.com>
2024-11-05 20:13:27 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,29 @@
-print ("Работа с Git")
+def add(x, y):
+    return x + y
+def subtract(x, y):
+    return x – y
+def multiply(x, y):
+    return x * y
+def divide(x, y):
+    if y != 0:
+        return x / y
+else:
+    return "Ошибка: деление на ноль"
+print("Выберите операцию:")
+print("1. Сложение")
+print("2. Вычитание")
+print("3. Умножение")
+print("4. Деление")
+choice = input("Введите номер операции (1/2/3/4): ")
+num1 = float(input("Введите первое число: "))
+num2 = float(input("Введите второе число: "))
+if choice == '1':
+    print(f"{num1} + {num2} = {add(num1, num2)}")
+elif choice == '2':
+    print(f"{num1} – {num2} = {subtract(num1, num2)}")
+elif choice == '3':
+    print(f"{num1} * {num2} = {multiply(num1, num2)}")
+elif choice = = '4':
+    print(f"{num1} / {num2} = {divide(num1, num2)}")
+else:
+    print("Неверный ввод")
